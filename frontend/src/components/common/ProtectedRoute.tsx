689402dadc90ccb1dd2e8446ee3b6da7b90f839a
Jack-Taunt <jack.taunt@hotmail.com>
2026-02-12 19:11:52 +1300

    create protected route that auto links to /login

---
+++ frontend/src/components/common/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "./AuthProvider";
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+    const { user } = useAuth();
+
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location.pathname}} replace />;
+    }
+
+    return children;
+};
+
+export default ProtectedRoute;
