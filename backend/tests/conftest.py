1fbeea7a7f3eb997f3be2cdcb823fbc25802e855
Jack-Taunt <jack.taunt@hotmail.com>
2026-02-24 13:35:42 +1300

    add default tags and more gardens for tests

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -5,7 +5,7 @@ from app.database import get_db, Base
 import pytest
 from fastapi.testclient import TestClient
 from app.auth.utils import hash_password
-from app.models import User, Garden
+from app.models import User, Garden, Tag
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
@@ -60,10 +60,31 @@ def auth_client(client, db_session):
 def default_data(db_session):
     """This fixture is used to initialise default data for use in testing"""
 
-    user = User(username= "default_user", email="default@email.com", password=hash_password("default_password"))
-    db_session.add(user)
+    user1 = User(username= "default_user1", email="default1@email.com", password=hash_password("Default_password1!"))
+    user2 = User(username= "default_user2", email="default2@email.com", password=hash_password("Default_password2!"))
+    db_session.add(user1)
+    db_session.add(user2)
+
+    default_tag_1 = Tag(name="default_tag_1")
+    default_tag_2 = Tag(name="default_tag_2")
+    default_tag_3 = Tag(name="default_tag_3")
+    default_tag_4 = Tag(name="default_tag_4")
+    default_tag_5 = Tag(name="default_tag_5")
+    default_tag_6 = Tag(name="default_tag_6")
+    db_session.add(default_tag_1)
+    db_session.add(default_tag_2)
+    db_session.add(default_tag_3)
+    db_session.add(default_tag_4)
+    db_session.add(default_tag_5)
+    db_session.add(default_tag_6)
 
     default_garden_1 = Garden(name="default_garden_1", description="default description 1", is_public=True, tags=[], user_id=1)
+    default_garden_2 = Garden(name="default_garden_2", description="default description 2", is_public=False, tags=[default_tag_1, default_tag_2, default_tag_3], user_id=1)
+    default_garden_3 = Garden(name="default_garden_3", description="default description 3", is_public=True, tags=[default_tag_1], user_id=2)
+    default_garden_4 = Garden(name="default_garden_4", description="default description 4", is_public=False, tags=[], user_id=2)
     db_session.add(default_garden_1)
+    db_session.add(default_garden_2)
+    db_session.add(default_garden_3)
+    db_session.add(default_garden_4)
 
     db_session.commit()
