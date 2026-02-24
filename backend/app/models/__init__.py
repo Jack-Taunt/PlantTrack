ef29aec73f1a9b89226e4fd589709d4bd08b5c13
Jack-Taunt <jack.taunt@hotmail.com>
2026-02-24 13:36:08 +1300

    update model init file with new models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,6 @@ from app.database import Base
 from .User import User
 from .Garden import Garden
 from .Plant import Plant
+from .Garden import Tag
+from .Garden import Section
+from .Garden import SectionPlant
