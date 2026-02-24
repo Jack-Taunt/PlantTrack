e9701a939385198c5a2c518e0caa507c0178abd3
Jack-Taunt <jack.taunt@hotmail.com>
2026-02-24 13:34:43 +1300

    set 5 tag limit on garden schema

--- backend/app/schemas/Garden.py
+++ backend/app/schemas/Garden.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, conlist
 from typing import Annotated, List
 from app.schemas.User import UserPublic
 
 class GardenCreate(BaseModel):
-    name: Annotated[str, Field(max_length=20)]
+    name: Annotated[str, Field(max_length=30)]
     description: Annotated[str, Field(max_length=256)]
     is_public: bool
-    tags: List[int]
+    tags: Annotated[list[int], Field(max_length=5)]
 
 
 class GardenOut(BaseModel):
