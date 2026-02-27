9ceab3bf5a92a4ac24e46231f147766dd866ae7c
Jack-Taunt <jack.taunt@hotmail.com>
2026-02-27 14:58:38 +1300

    garden page shows name, description, tags

--- frontend/src/components/Garden/garden.tsx
+++ frontend/src/components/Garden/garden.tsx
@@ -3,32 +3,18 @@ import Navbar from "../common/Navbar";
 import { useState, useEffect } from "react";
 import { type Garden } from "../../types/garden";
 import api from "../../client/client"
-import { type Tag } from "../../types/garden";
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import TagList from "./tag-list";
 
 
 const GardenPage = () => {
     const gardenId = useParams().gardenId;
     const [garden, setGarden] = useState<Garden | null>(null);
-    const [tags, setTags] = useState<Tag[]>([]);
-    
-
 
     const fetchGarden = async () => {
         try {
             const garden = await api.get("/gardens/" + gardenId)
             setGarden(garden.data)
-            
-        } catch (err: any) {
-            console.log(err)
-        }
-    }
-
-
-    const fetchGardenTags = async () => {
-        try {
-            const tags = await api.get("/gardens/tags")
-            setTags(tags.data)
         } catch (err: any) {
             console.log(err)
         }
@@ -36,16 +22,28 @@ const GardenPage = () => {
 
     useEffect(() => {
         fetchGarden()
-        fetchGardenTags()
     }, []);
 
 
     return (
+        
         <>
             <Navbar/>
-            <Typography>
-                {garden?.name}
-            </Typography>
+            {garden && (
+                <div>
+                    <Typography variant="h3" sx={{fontWeight: 'bold', textAlign: 'center', pt: 4}}>
+                        {garden.name}
+                    </Typography>
+                    <Typography variant="h5" sx={{textAlign: 'center', pt: 4}}>
+                        {garden.description}
+                    </Typography>
+                    <Box sx={{display: "flex", mx: 'auto', justifyContent: 'center'}}>
+                        {garden.tags &&
+                            <TagList tags={garden.tags}/>
+                        }
+                    </Box>
+                </div>
+            )}
         </>
     )
 };
