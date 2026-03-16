22e9a52b04ae7ed260727a08ad49a458cebb77f7
Jack-Taunt <jack.taunt@hotmail.com>
2026-03-16 17:08:15 +1300

    remove unused code

--- backend/app/crud/garden.py
+++ backend/app/crud/garden.py
@@ -62,5 +62,5 @@ def get_garden_tags_db(db: Session):
 
 
 def delete_garden_db(garden_id: int, db: Session):
-    garden = db.query(Garden).filter(Garden.id == garden_id).delete()
-    # db.delete(garden)
+    db.query(Garden).filter(Garden.id == garden_id).delete()
+    
