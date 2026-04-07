a200be3f8955d2d34b70f7e24bc9c0476edf3a17
Jack-Taunt <jack.taunt@hotmail.com>
2026-04-07 20:19:49 +1200

    add mui masonry component for gardenPlant info sections

--- frontend/src/components/Plant/GardenPlant.tsx
+++ frontend/src/components/Plant/GardenPlant.tsx
@@ -192,107 +192,110 @@ const GardenPlantInfo = ({gardenId, plantId, userOwns}: GardenPlantProps) => {
                                         </Typography>
                                     </Box>
                                 </Grid>
-                                <Grid size={6}>
-                                    <PlantSection title="General Information">
-                                        <PlantField label={"Family"} value={gardenPlant.plant.family}/>
-                                        <PlantField label={"Genus"} value={gardenPlant.plant.genus}/>
-                                        <PlantField label={"Class"} value={gardenPlant.plant.class_}/>
-                                        <PlantField label={"Order"} value={gardenPlant.plant.order}/>
-                                        <PlantField label={"Phylum"} value={gardenPlant.plant.phylum}/>
-                                    </PlantSection>
-                                </Grid>
-                                {gardenPlant.plant.toxicity && (
+                                <Masonry columns={2} spacing={2}>
                                     <Grid size={6}>
-                                        <PlantSection title="Toxicity">
-                                            <PlantField label={"Toxicity"} value={String(gardenPlant.plant.toxicity.toxicity)}/>
-                                            {gardenPlant.plant.toxicity.toxicity !== "non_toxic" && (
-                                                <Stack gap={1} direction={"row"}>
-                                                    <PlantBoolField label={"Toxic to Humans"} value={gardenPlant.plant.toxicity.toxic_to_humans}/>
-                                                    <PlantBoolField label={"Toxic to Cats"} value={gardenPlant.plant.toxicity.toxic_to_cats}/>
-                                                    <PlantBoolField label={"Toxic to Dogs"} value={gardenPlant.plant.toxicity.toxic_to_dogs}/>
-                                                </Stack>
-                                            )}
+                                        <PlantSection title="General Information">
+                                            <PlantField label={"Family"} value={gardenPlant.plant.family}/>
+                                            <PlantField label={"Genus"} value={gardenPlant.plant.genus}/>
+                                            <PlantField label={"Class"} value={gardenPlant.plant.class_}/>
+                                            <PlantField label={"Order"} value={gardenPlant.plant.order}/>
+                                            <PlantField label={"Phylum"} value={gardenPlant.plant.phylum}/>
                                         </PlantSection>
                                     </Grid>
-                                )}
-                                {gardenPlant.plant.edibility && (
-                                    <Grid size={6}>
-                                        <PlantSection title="Edibility">
-                                            {!gardenPlant.plant.edibility.edible_fruit && 
-                                            !gardenPlant.plant.edibility.edible_leaves && 
-                                            !gardenPlant.plant.edibility.edible_flowers &&
-                                            !gardenPlant.plant.edibility.edible_roots ? (
-                                                <PlantBoolField label={"Not Edible"} value={true}/>
-                                            ) : (
-                                                <Stack gap={1} direction={"row"}>
-                                                    <PlantBoolField label={"Edible Fruit"} value={gardenPlant.plant.edibility.edible_fruit}/>
-                                                    <PlantBoolField label={"Edible Leaves"} value={gardenPlant.plant.edibility.edible_leaves}/>
-                                                    <PlantBoolField label={"Edible Flowers"} value={gardenPlant.plant.edibility.edible_flowers}/>
-                                                    <PlantBoolField label={"Edible Roots"} value={gardenPlant.plant.edibility.edible_roots}/>
-                                                </Stack>
-                                            )}
-                                        </PlantSection>
-                                    </Grid>
-                                )}
-                                {gardenPlant.plant.planting && (
-                                    <Grid size={6}>
-                                        <PlantSection title="Planting">
-                                            <Stack gap={1} direction={"row"}>
-                                                <PlantBoolField label={"Can Direct Sow"} value={gardenPlant.plant.planting.direct_sow}/>
-                                                <PlantBoolField label={"Can Transplant"} value={gardenPlant.plant.planting.transplant}/>
-                                                <PlantBoolField label={"Can Propagate"} value={gardenPlant.plant.planting.propagation}/>
-                                            </Stack>
-                                            <PlantField label={"Seed Planting Depth (cm)"} value={String(gardenPlant.plant.planting.seed_depth)}/>
-                                            <PlantField label={"Plant Spacing (cm)"} value={String(gardenPlant.plant.planting.spacing)}/>
-                                        </PlantSection>
-                                    </Grid>
-                                )}
-                                {gardenPlant.plant.growth && (
-                                    <Grid size={6}>
-                                        <PlantSection title="Plant Growth">
-                                            <Stack gap={1} direction={"row"}>
-                                                <PlantBoolField label={"Annual"} value={gardenPlant.plant.growth.annual}/>
-                                                <PlantBoolField label={"Biennial"} value={gardenPlant.plant.growth.biennial}/>
-                                                <PlantBoolField label={"Perennial"} value={gardenPlant.plant.growth.perennial}/>
-                                            </Stack>
-                                            <PlantField label={"Growth Rate"} value={String(gardenPlant.plant.growth.growth_rate)}/>
-                                            <PlantRangeField label={"Growth Width"} min={gardenPlant.plant.growth.min_width} max={gardenPlant.plant.growth.max_width}/>
-                                            <PlantRangeField label={"Growth Height"} min={gardenPlant.plant.growth.min_height} max={gardenPlant.plant.growth.max_height}/>
-                                            <PlantRangeField label={"Harvest Time"} min={gardenPlant.plant.growth.min_days_to_harvest} max={gardenPlant.plant.growth.max_days_to_harvest}/>
-                                        </PlantSection>
-                                    </Grid>
-                                )}
-                                {gardenPlant.plant.environment && (
-                                    <Grid size={6}>
-                                        <PlantSection title="Plant Environment">
-                                            <PlantField label={"Light Type"} value={String(gardenPlant.plant.environment.light_type)}/>
-                                            <PlantRangeField label={"Temperature"} min={gardenPlant.plant.environment.min_temp} max={gardenPlant.plant.environment.max_temp}/>
-                                            <PlantRangeField label={"Humidity"} min={gardenPlant.plant.environment.min_humidity} max={gardenPlant.plant.environment.max_humidity}/>
-                                            <PlantRangeField label={"usda Zone"} min={gardenPlant.plant.environment.min_usda_zone} max={gardenPlant.plant.environment.max_usda_zone}/>
-                                        </PlantSection>
-                                    </Grid>
-                                )}
-                                {gardenPlant.plant.care_requirements && (
-                                    <Grid size={6}>
-                                        <PlantSection title="Plant Care">
-                                            <Subsection title="Soil"/>
-                                            <PlantField label={"Soil Type"} value={String(gardenPlant.plant.care_requirements.soil_type)}/>
-                                            <PlantField label={"Soil Moisture"} value={String(gardenPlant.plant.care_requirements.soil_moisture)}/>
-                                            <PlantRangeField label={"Soil Ph"} min={gardenPlant.plant.care_requirements.min_soil_ph} max={gardenPlant.plant.care_requirements.max_soil_ph}/>
-                                            
-                                            <Subsection title="Water"/>
-                                            <PlantBoolField label={"Drought Tolerant"} value={gardenPlant.plant.care_requirements.drought_tolerant}/>
-                                            <PlantRangeField label={"Water Frequency"} min={gardenPlant.plant.care_requirements.min_water_frequency} max={gardenPlant.plant.care_requirements.max_water_frequency}/>
+                                    {gardenPlant.plant.care_requirements && (
+                                        <Grid size={6}>
+                                            <PlantSection title="Plant Care">
+                                                <Subsection title="Soil"/>
+                                                <PlantField label={"Soil Type"} value={String(gardenPlant.plant.care_requirements.soil_type)}/>
+                                                <PlantField label={"Soil Moisture"} value={String(gardenPlant.plant.care_requirements.soil_moisture)}/>
+                                                <PlantRangeField label={"Soil Ph"} min={gardenPlant.plant.care_requirements.min_soil_ph} max={gardenPlant.plant.care_requirements.max_soil_ph}/>
+                                                
+                                                <Subsection title="Water"/>
+                                                <PlantBoolField label={"Drought Tolerant"} value={gardenPlant.plant.care_requirements.drought_tolerant}/>
+                                                <PlantRangeField label={"Water Frequency"} min={gardenPlant.plant.care_requirements.min_water_frequency} max={gardenPlant.plant.care_requirements.max_water_frequency}/>
 
-                                            
-                                            <Subsection title="Fertilizer"/>
-                                            <PlantField label={"Fertilizer Frequency"} value={String(gardenPlant.plant.care_requirements.fertilizer_frequency)}/>
-                                            <PlantField label={"Nitrogen"} value={String(gardenPlant.plant.care_requirements.fertilizer_nitrogen)}/>
-                                            <PlantField label={"Phosphorus"} value={String(gardenPlant.plant.care_requirements.fertilizer_phosphorus)}/>
-                                            <PlantField label={"Potassium"} value={String(gardenPlant.plant.care_requirements.fertilizer_potassium)}/>
-                                        </PlantSection>
-                                    </Grid>
-                                )}
+                                                
+                                                <Subsection title="Fertilizer"/>
+                                                <PlantField label={"Fertilizer Frequency"} value={String(gardenPlant.plant.care_requirements.fertilizer_frequency)}/>
+                                                <PlantField label={"Nitrogen"} value={String(gardenPlant.plant.care_requirements.fertilizer_nitrogen)}/>
+                                                <PlantField label={"Phosphorus"} value={String(gardenPlant.plant.care_requirements.fertilizer_phosphorus)}/>
+                                                <PlantField label={"Potassium"} value={String(gardenPlant.plant.care_requirements.fertilizer_potassium)}/>
+                                            </PlantSection>
+                                        </Grid>
+                                    )}
+                                    {gardenPlant.plant.environment && (
+                                        <Grid size={6}>
+                                            <PlantSection title="Plant Environment">
+                                                <PlantField label={"Light Type"} value={String(gardenPlant.plant.environment.light_type)}/>
+                                                <PlantRangeField label={"Temperature"} min={gardenPlant.plant.environment.min_temp} max={gardenPlant.plant.environment.max_temp}/>
+                                                <PlantRangeField label={"Humidity"} min={gardenPlant.plant.environment.min_humidity} max={gardenPlant.plant.environment.max_humidity}/>
+                                                <PlantRangeField label={"usda Zone"} min={gardenPlant.plant.environment.min_usda_zone} max={gardenPlant.plant.environment.max_usda_zone}/>
+                                            </PlantSection>
+                                        </Grid>
+                                    )}
+                                    
+                                    {gardenPlant.plant.growth && (
+                                        <Grid size={6}>
+                                            <PlantSection title="Plant Growth">
+                                                <Stack gap={1} direction={"row"}>
+                                                    <PlantBoolField label={"Annual"} value={gardenPlant.plant.growth.annual}/>
+                                                    <PlantBoolField label={"Biennial"} value={gardenPlant.plant.growth.biennial}/>
+                                                    <PlantBoolField label={"Perennial"} value={gardenPlant.plant.growth.perennial}/>
+                                                </Stack>
+                                                <PlantField label={"Growth Rate"} value={String(gardenPlant.plant.growth.growth_rate)}/>
+                                                <PlantRangeField label={"Growth Width"} min={gardenPlant.plant.growth.min_width} max={gardenPlant.plant.growth.max_width}/>
+                                                <PlantRangeField label={"Growth Height"} min={gardenPlant.plant.growth.min_height} max={gardenPlant.plant.growth.max_height}/>
+                                                <PlantRangeField label={"Harvest Time"} min={gardenPlant.plant.growth.min_days_to_harvest} max={gardenPlant.plant.growth.max_days_to_harvest}/>
+                                            </PlantSection>
+                                        </Grid>
+                                    )}
+                                    {gardenPlant.plant.planting && (
+                                        <Grid size={6}>
+                                            <PlantSection title="Planting">
+                                                <Stack gap={1} direction={"row"}>
+                                                    <PlantBoolField label={"Can Direct Sow"} value={gardenPlant.plant.planting.direct_sow}/>
+                                                    <PlantBoolField label={"Can Transplant"} value={gardenPlant.plant.planting.transplant}/>
+                                                    <PlantBoolField label={"Can Propagate"} value={gardenPlant.plant.planting.propagation}/>
+                                                </Stack>
+                                                <PlantField label={"Seed Planting Depth (cm)"} value={String(gardenPlant.plant.planting.seed_depth)}/>
+                                                <PlantField label={"Plant Spacing (cm)"} value={String(gardenPlant.plant.planting.spacing)}/>
+                                            </PlantSection>
+                                        </Grid>
+                                    )}
+                                    {gardenPlant.plant.toxicity && (
+                                        <Grid size={6}>
+                                            <PlantSection title="Toxicity">
+                                                <PlantField label={"Toxicity"} value={String(gardenPlant.plant.toxicity.toxicity)}/>
+                                                {gardenPlant.plant.toxicity.toxicity !== "non_toxic" && (
+                                                    <Stack gap={1} direction={"row"}>
+                                                        <PlantBoolField label={"Toxic to Humans"} value={gardenPlant.plant.toxicity.toxic_to_humans}/>
+                                                        <PlantBoolField label={"Toxic to Cats"} value={gardenPlant.plant.toxicity.toxic_to_cats}/>
+                                                        <PlantBoolField label={"Toxic to Dogs"} value={gardenPlant.plant.toxicity.toxic_to_dogs}/>
+                                                    </Stack>
+                                                )}
+                                            </PlantSection>
+                                        </Grid>
+                                    )}
+                                    {gardenPlant.plant.edibility && (
+                                        <Grid size={6}>
+                                            <PlantSection title="Edibility">
+                                                {!gardenPlant.plant.edibility.edible_fruit && 
+                                                !gardenPlant.plant.edibility.edible_leaves && 
+                                                !gardenPlant.plant.edibility.edible_flowers &&
+                                                !gardenPlant.plant.edibility.edible_roots ? (
+                                                    <PlantBoolField label={"Not Edible"} value={true}/>
+                                                ) : (
+                                                    <Stack gap={1} direction={"row"}>
+                                                        <PlantBoolField label={"Edible Fruit"} value={gardenPlant.plant.edibility.edible_fruit}/>
+                                                        <PlantBoolField label={"Edible Leaves"} value={gardenPlant.plant.edibility.edible_leaves}/>
+                                                        <PlantBoolField label={"Edible Flowers"} value={gardenPlant.plant.edibility.edible_flowers}/>
+                                                        <PlantBoolField label={"Edible Roots"} value={gardenPlant.plant.edibility.edible_roots}/>
+                                                    </Stack>
+                                                )}
+                                            </PlantSection>
+                                        </Grid>
+                                    )}
+                                </Masonry>
                             </Grid>
                         </Grid>
                     </Paper>
